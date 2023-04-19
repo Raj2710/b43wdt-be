8f6e1b5ef25e97fd63203a370e52a0cc556d9cf8
Raj2710 <nagarajansai2727@gmail.com>
2023-04-19 08:53:12 +0530

    remove jade

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ var dashboardRouter = require('./routes/dashboard')
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
